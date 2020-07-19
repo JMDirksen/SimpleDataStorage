370f5a2253a4d411c7da4de2c0a1866d2d6be68c
Jefta Dirksen <jeftadirksen@gmail.com>
2020-07-19 11:31:36 +0200

    Output with EOL

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
             $filename = strtolower($filename).".txt";
             file_put_contents($filename, $data.PHP_EOL, FILE_APPEND);
             $proto = $_SERVER['HTTPS'] == "on" ? "https://" : "http://";
-            echo $proto.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].$filename;
+            echo $proto.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].$filename.PHP_EOL;
         }
     }
 ?>
