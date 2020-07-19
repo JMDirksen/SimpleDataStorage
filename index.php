cf3962f352f188a2020f4799d5c9f35be81176f5
Jefta Dirksen <jeftadirksen@gmail.com>
2020-07-19 13:16:07 +0200

    Error handling

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <?php
-    if(count($_POST)==1) {
+    if(isset($_POST) && count($_POST)) {
         foreach($_POST as $filename=>$data) {
-            if(!ctype_alnum($filename)) die("Filename must be alphanumeric");
+            if(!ctype_alnum($filename)) {
+                echo "Error: Filename must be alphanumeric";
+                continue;
+            }
             $filename = strtolower($filename).".txt";
-            file_put_contents($filename, $data.PHP_EOL, FILE_APPEND);
-            $proto = $_SERVER['HTTPS'] == "on" ? "https://" : "http://";
-            echo $proto.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].$filename.PHP_EOL;
+            if(@file_put_contents($filename, $data.PHP_EOL, FILE_APPEND)) {
+                $proto = $_SERVER['HTTPS'] == "on" ? "https://" : "http://";
+                echo $proto.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].$filename.PHP_EOL;
+            }
+            else {
+                echo "Error: File write error".PHP_EOL;
+            }
         }
     }
 ?>
